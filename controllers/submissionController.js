c960848a14e1af8bc4e1bb93e88abac60a7c2c0e
Đoàn Thu Ngân <55989458+thungan1909@users.noreply.github.com>
2025-03-28 17:00:50 +0700

    commit

--- controllers/submissionController.js
+++ controllers/submissionController.js
@@ -163,12 +163,8 @@ const submissionController = {
             await Promise.all([submissionPromise, lessonUpdatePromise, userUpdatePromise]);
 
             return res.status(200).json({
-<<<<<<< HEAD
                 message: "Submission received successfully",
                 ...submissionResponse
-=======
-                ...result, score,
->>>>>>> e75fd1e69facd542a7c18b16cc19e021a72dc38e
             });
         } catch (err) {
             console.error("Error in listen Lesson:", err);
