66246166a3a341fd22fe08c14c9a3a94579d5720
Đoàn Thu Ngân <55989458+thungan1909@users.noreply.github.com>
2025-09-10 21:56:46 +0700

    update import bcryptjs

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
+
 
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
